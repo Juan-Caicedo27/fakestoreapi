70adc8aa0d83caccf26005d5dc784f5a7d9374a7
Juan-Caicedo27 <juan.caicedoc@uniagustiniana.edu.co>
2025-05-27 23:22:35 +0000

    Convertido a PWA

--- Script.js
+++ Script.js
@@ -164,3 +164,10 @@ function renderInfoTab() {
       </div>
   `;
 }
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker registrado'))
+    .catch(error => console.error('Error al registrar el Service Worker', error));
+}
